8b53ef7a7a276cf844118c186b980c96f962d49b
ejaay-tester <ejaaay.tester@gmail.com>
2026-03-23 18:20:58 +0800

    test(api): refactor POST todos test to use registerUser fixture

--- tests/api/todos.api.test.ts
+++ tests/api/todos.api.test.ts
@@ -3,53 +3,34 @@ import { test, expect } from "../fixtures/auth.fixture"
 
 test.describe("Todo API Tests", () => {
   // Create a new todo
-  test("Should create a new todo", async ({ request }) => {
-    // Step 1: Register a new user
-    // Method: POST
-    // Endpoint: api/auth/register
-
-    console.log("Registering user...")
-    const registerResponse = await request.post(
-      "http://localhost:3000/api/auth/register",
+  test.only("Should create a new todo", async ({ registeredUser, request }) => {
+    console.log("Creating new todo...")
+    const createTodoResponse = await request.post(
+      "http://localhost:3000/api/todos",
       {
+        headers: {
+          Authorization: `Bearer ${registeredUser.token}`,
+        },
         data: {
-          email: `testuser14@yopmail.com`, // Unique email using timestamp
-          password: `TestP@ssword123`,
+          title: "Second todo title",
+          description: "Second todo description",
+          completed: false,
         },
       },
     )
 
-    console.log("Register Status:", registerResponse.status())
-    expect(registerResponse.status()).toBe(201)
+    console.log("Todo Status:", createTodoResponse.status())
+    expect(createTodoResponse.status()).toBe(201)
 
-    const registerData = await registerResponse.json()
-    console.log("Register Data:", registerData)
-    const token = registerData.data.token
+    const createTodoBody = await createTodoResponse.json()
+    console.log("Todo Data:", createTodoBody)
+    // const createdTodoData = createTodoBody.data
 
-    // Step 2: Create a new todo with authentication
-    // Method: POST
-    // Endpoint: api/todos
-    console.log("Creating todo...")
-    const response = await request.post("http://localhost:3000/api/todos", {
-      headers: { Authorization: `Bearer ${token}` },
-      data: {
-        title: "First todo title",
-        description: "first todo description",
-        completed: "false",
-      },
-    })
-    console.log("Todo Status:", response.status())
-    expect(response.status()).toBe(201)
-
-    const todoResponse = await response.json()
-    console.log("Todo Data:", todoResponse)
-    // const todo = todoResponse.data
-
-    expect(todoResponse.data).toHaveProperty("_id")
-    expect(todoResponse.data.title).toBe("First todo title")
+    expect(createTodoBody.data).toHaveProperty("_id")
+    expect(createTodoBody.data.title).toBe("Second todo title")
   })
 
-  test.only("Should get all todos", async ({ authenticatedRequest }) => {
+  test("Should get all todos", async ({ authenticatedRequest }) => {
     // Fetch all todos with auth token
     // Method: POST
     // Endpoint: api/todos
