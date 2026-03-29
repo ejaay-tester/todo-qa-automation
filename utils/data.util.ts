9838fc217650fc82ff30bfb3df979534c25cd62e
ejaay-tester <ejaaay.tester@gmail.com>
2026-03-29 17:49:28 +0800

    util(data.util): add unique data generator

---
+++ utils/data.util.ts
@@ -0,0 +1,19 @@
+/**
+ * UNIQUE DATA GENERATOR
+ * Example Outputs:
+ * Title: "Buy Groceries 9K2X1"
+ * Description: "Task details for AF82Q"
+ */
+const generateUniqueString = (prefix: string): string => {
+  // Math.random(): generates a random decimal (e.g., 0.123456...)
+  // .toString(36): converts that number to Base-36(uses 0-9 and a-z), turning the decimal
+  // into string of random letters/numbers
+  // .toUpperCase(): makes it look like a clean ID (e.g., A1B2C)
+  // .slice(): skips the 0. at the start of the decimal and grabs exactly 5 characters
+  const randomSuffix = Math.random().toString(36).toUpperCase().slice(2, 7)
+  return `${prefix} ${randomSuffix}`
+}
+
+// export function generateUniqueString(prefix: string) {
+//   return `${prefix}-${Date.now()}-${Math.random().toString(36).substring(2, 8)}`
+// }
