f23d7066602354609305bae098552bd954a1f842
ejaay-tester <ejaaay.tester@gmail.com>
2026-04-08 17:24:36 +0800

    test(TodoClient): create helper class to centralize CRUD logic

---
+++ src/api/TodoClient.ts
@@ -0,0 +1,38 @@
+import { APIRequestContext, expect } from "@playwright/test"
+
+export class TodoController {
+  constructor(private request: APIRequestContext) {}
+
+  async createTodo(payload: any) {
+    const response = await this.request.post("/api/todos", { data: payload })
+    expect(response.status(), "POST /api/todos should return 201").toBe(201)
+    return (await response.json()).data
+  }
+
+  async updateTodo(id: string, payload: any) {
+    const response = await this.request.put(`/api/todos/${id}`, {
+      data: payload,
+    })
+    expect(response.status(), "PUT /api/todos/:id should return 200").toBe(200)
+    return (await response.json()).data
+  }
+
+  async getAllTodo() {
+    const response = await this.request.get("/api/todos")
+    expect(response.status(), "GET /api/todos should return 200").toBe(200)
+    return (await response.json()).data
+  }
+
+  async getTodo(id: string) {
+    const response = await this.request.get(`/api/todos/${id}`)
+    expect(response.status(), "GET /api/todos/:id should return 200").toBe(200)
+    return (await response.json()).data
+  }
+
+  async deleteTodo(id: string) {
+    const response = await this.request.delete(`/api/todos/${id}`)
+    expect(response.status(), "DELETE /api/todos/:id should return 204").toBe(
+      204,
+    )
+  }
+}
