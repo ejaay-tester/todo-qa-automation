6dd1bdfc73c4637380d43eeda5aae2f0861eb3b0
ejaay-tester <ejaaay.tester@gmail.com>
2026-04-09 21:35:53 +0800

    chore(TodoClient): rename async instance methods

--- src/api/TodoClient.ts
+++ src/api/TodoClient.ts
@@ -3,13 +3,13 @@ import { APIRequestContext, expect } from "@playwright/test"
 export class TodoClient {
   constructor(private request: APIRequestContext) {}
 
-  async createTodo(payload: any) {
+  async create(payload: any) {
     const response = await this.request.post("/api/todos", { data: payload })
     expect(response.status(), "POST /api/todos should return 201").toBe(201)
     return (await response.json()).data
   }
 
-  async updateTodo(id: string, payload: any) {
+  async update(id: string, payload: any) {
     const response = await this.request.put(`/api/todos/${id}`, {
       data: payload,
     })
@@ -17,19 +17,19 @@ export class TodoClient {
     return (await response.json()).data
   }
 
-  async getAllTodo() {
+  async getAll() {
     const response = await this.request.get("/api/todos")
     expect(response.status(), "GET /api/todos should return 200").toBe(200)
     return (await response.json()).data
   }
 
-  async getTodo(id: string) {
+  async get(id: string) {
     const response = await this.request.get(`/api/todos/${id}`)
     expect(response.status(), "GET /api/todos/:id should return 200").toBe(200)
     return (await response.json()).data
   }
 
-  async deleteTodo(id: string) {
+  async delete(id: string) {
     const response = await this.request.delete(`/api/todos/${id}`)
     expect(response.status(), "DELETE /api/todos/:id should return 204").toBe(
       204,
