443bac71243528bb2eaf91770a16263c62af9c9c
ejaay-tester <ejaaay.tester@gmail.com>
2026-04-09 22:57:46 +0800

    chore(config): change path of testDir to ./src/tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test"
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./src/tests",
   fullyParallel: true,
   reporter: "html",
   use: {
