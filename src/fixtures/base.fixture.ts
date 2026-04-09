9a8a070f874e8ac3037e81e676270c3008d35dc3
ejaay-tester <ejaaay.tester@gmail.com>
2026-04-09 21:03:20 +0800

    test(todo-fixture): create custom fixture to connect the auth-fixture and TodoClient

---
+++ src/fixtures/base.fixture.ts
@@ -0,0 +1,18 @@
+import { authTest } from "./auth.fixture"
+import { TodoClient } from "../api/TodoClient"
+
+// Define the type for the new fixture you are adding
+type TodoFixtures = {
+  todoClient: TodoClient
+}
+
+// Extend the base test to include the custom classes
+export const test = authTest.extend<TodoFixtures>({
+  todoClient: async ({ authenticatedRequest }, use) => {
+    // authenticatedRequest comes from the AuthFixture
+    const client = new TodoClient(authenticatedRequest)
+    await use(client)
+  },
+})
+
+export { expect } from "@playwright/test"
