ec63b4e7eb9cf67cf893de5e76bef17e28103557
ejaay-tester <ejaaay.tester@gmail.com>
2026-05-10 18:14:12 +0800

    chore(config): enable junit reporter in the config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     ["line"],
     ["html", { outputFolder: "playwright-report", open: "never" }],
     ["allure-playwright", { outputFolder: "allure-results" }],
+    ["junit", { outputFile: "allure-results/results.xml" }],
   ],
 
   use: {
