7f7928abd3395b397ec478f248c3f1e60be91ca4
ejaay-tester <ejaaay.tester@gmail.com>
2026-05-25 17:02:44 +0800

    refactor(test-data): extract generateUser factory and EXPIRED_JWT constant

---
+++ src/test-data/constants.ts
@@ -0,0 +1,4 @@
+export const EXPIRED_JWT =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+  ".eyJpZCI6IjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2IiwiaWF0IjoxNjAwMDAwMDAwLCJleHAiOjE2MDAwMDAwMDF9" +
+  ".invalidsignature"
