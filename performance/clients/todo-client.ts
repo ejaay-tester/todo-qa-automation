179c22e7bf1729c893abd73ac208f80cf42ae2b7
ejaay-tester <ejaaay.tester@gmail.com>
2026-06-05 18:34:46 +0800

    feat(perf): add k6 todo client class

---
+++ performance/clients/todo-client.ts
@@ -0,0 +1,48 @@
+import http, { RefinedResponse, ResponseType, Params } from "k6/http"
+import { TodoPayload } from "../../src/types/todo.type"
+
+const BASE_URL: string = __ENV.BASE_URL || "http://localhost:3000"
+
+export class TodoClient {
+  private readonly params: Params
+
+  constructor(token: string) {
+    this.params = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  }
+
+  create(payload: TodoPayload): RefinedResponse<ResponseType> {
+    return http.post(
+      `${BASE_URL}/api/todos`,
+      JSON.stringify(payload),
+      this.params, // pass the whole params object
+    )
+  }
+
+  getAll(): RefinedResponse<ResponseType> {
+    return http.get(`${BASE_URL}/api/todos`, this.params)
+  }
+
+  getById(id: string): RefinedResponse<ResponseType> {
+    return http.get(`${BASE_URL}/api/todos/${id}`, this.params)
+  }
+
+  update(
+    id: string,
+    payload: Partial<TodoPayload>,
+  ): RefinedResponse<ResponseType> {
+    return http.put(
+      `${BASE_URL}/api/todos/${id}`,
+      JSON.stringify(payload),
+      this.params,
+    )
+  }
+
+  delete(id: string): RefinedResponse<ResponseType> {
+    return http.del(`${BASE_URL}/api/todos/${id}`, null, this.params)
+  }
+}
